c96cad5526479f38d974619bc8b849276d66fcfa
elimuconnect <wamithindegwa1@gmail.com>
2026-09-25 01:12:20 -0700

    Update trial7.js

--- trial7.js
+++ trial7.js
@@ -31777,11 +31777,6 @@ async function downloadGeneratedTimetablesWord() {
 }
 
 
-<script>
-console.log("DOCX library:", typeof docx);
-console.log("DOCX Document:", typeof docx?.Document);
-console.log("DOCX Packer:", typeof docx?.Packer);
-</script>
 
 
 
